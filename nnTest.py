000f9e0df9851aed8eb8a13d5f95e2e5631eb171
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-12-06 23:26:40 +0000

    updated for lightGBM

--- nnTest.py
+++ nnTest.py
@@ -19,6 +19,7 @@ from keras.layers import Dense
 from keras.wrappers.scikit_learn import KerasRegressor
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
+import lightgbm as lgb
 # load dataset
 SHAPE = 0
 def baseline_model(SHAPE):
@@ -27,7 +28,7 @@ def baseline_model(SHAPE):
         model = Sequential()
         print(f"{SHAPE}")
         model.add(Dense(SHAPE, input_dim=SHAPE, kernel_initializer='normal', activation='relu'))
-        model.add(Dense(10, kernel_initializer='normal', activation='relu'))
+        model.add(Dense(1250, kernel_initializer='normal', activation='relu'))
         # model.add(Dense(10, kernel_initializer='normal', activation='relu'))
         model.add(Dense(1, kernel_initializer='normal'))
         
@@ -49,16 +50,16 @@ def linearRegressionOneHot(filename):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.10, random_state = 1)
     print("Now fitting")
     print(np.shape(X_train)[1])
-    model = KerasRegressor(build_fn=baseline_model(SHAPE), epochs=100, batch_size=5, verbose=0)
-
+    #model = KerasRegressor(build_fn=baseline_model(SHAPE), epochs=100, batch_size=5, verbose=0)
+    model = lgb.LGBMRegressor()
     # kfold = KFold(n_splits=10)
     # results = cross_val_score(estimator, X, Y, cv=kfold)
     # print("Baseline: %.2f (%.2f) MSE" % (results.mean(), results.std()))
-    # model.fit(X_train, y_train)
-    results = cross_val_score(model, X_train, y_train, scoring='r2')
-    # score = model.score(X_test, y_test)
-    print(f"The Score on the test set is {results}")
-    score = cross_val_score(model, X_test, y_test, scoring='r2')
+    model.fit(X_train, y_train,verbose=True)
+    #results = cross_val_score(model, X_train, y_train, scoring='r2')
+    score = model.score(X_test, y_test)
+    print(f"The Score on the test set is {score}")
+    score = model.score(X_train, y_train)
     print(f"The Score on the train set is {score}")
     #scores = []
     # kfold = KFold(n_splits=3, shuffle=True, random_state=42)
@@ -67,7 +68,6 @@ def linearRegressionOneHot(filename):
     #     score = model.score(X.iloc[test,:], y.iloc[test,:])
     #     scores.append(score)
     # print(scores)
-
 def main():
     linearRegressionOneHot("CleanData/true_car_listings.csv")
 
