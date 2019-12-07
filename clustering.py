48d6487b796526b08b84accb1d904951970afdde
Varun Nayak <varunnayak3101@gmail.com>
2019-12-06 16:17:09 -0800

    clustering com

--- clustering.py
+++ clustering.py
@@ -10,15 +10,34 @@ import matplotlib.pyplot as plt
 from scipy import stats
 from sklearn import preprocessing
 from sklearn.model_selection import KFold, train_test_split
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans, MiniBatchKMeans
+from sklearn.linear_model import LinearRegression
+
+NUM_CLUSTERS = 6
 
 def kmeans(filename):
     data = generateOneHotDataFrame(filename)
     X = pd.DataFrame(data.drop(['Price'],axis=1))
     y = pd.DataFrame(data['Price'])
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.10, random_state = 1)
-    kmeans = KMeans(n_clusters = 8, random_state = 0).fit(X_train)
-    print(kmeans.predict(X_test))
+    kmeans = MiniBatchKMeans(n_clusters = NUM_CLUSTERS , random_state = 0)
+    kmeans.fit(X_train)
+    carClusterNumber = kmeans.predict(X)
+    data['ClusterNumber'] = carClusterNumber
+    
+    for clusterNumber in range(NUM_CLUSTERS):
+        dataForThisCluster = (data['ClusterNumber'] == clusterNumber)
+        X_c = pd.DataFrame(dataForThisCluster.drop(['Price'],axis=1))
+        y_c = pd.DataFrame(dataForThisCluster['Price'])
+        X_train_c, X_test_c, y_train_c, y_test_c = train_test_split(X_c, y_c, test_size = 0.10, random_state = 1)
+        model = LinearRegression()
+        model.fit(X_train_c, y_train_c)
+        score = model.score(X_test_c, y_test_c)
+        print(f"The Score on the test set is {score}")
+        score = model.score(X_train_c, y_train_c)
+        print(f"The Score on the train set is {score}")
+    pass
+        
     
     # X = np.array([[1, 2], [1, 4], [1, 0],[10, 2], [10, 4], [10, 0]])
     # kmeans = KMeans(n_clusters=2, random_state=0).fit(X)
